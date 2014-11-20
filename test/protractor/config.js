53ffd8adb5ca5d14c358be5e4782c887134eb26f
Kamil Skowron <git@hedonsoftware.com>
2014-11-20 20:13:18 +0000

    Protractor tests updated

--- test/protractor/config.js
+++ test/protractor/config.js
@@ -11,8 +11,8 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
     'login.js',
-    'boards.js',
-    'projects.js',
+    // 'boards.js',
+    // 'projects.js',
   ],
   capabilities : {
     browserName : 'chrome',
--- test/protractor/login.js
+++ test/protractor/login.js
@@ -23,9 +23,27 @@ describe('KogoAdmin login screen', function () {
     expect(element(by.model('form.username')).isPresent()).toBe(true);
     expect(element(by.model('form.password')).isPresent()).toBe(true);
     expect(element(by.css('form .footer button')).isPresent()).toBe(true);
+
+    // error message shouldn't be there
+    expect(element(by.css('form .alert')).isDisplayed()).toBe(false);
+  });
+
+  it ('shouldn\'t allow to login with incorrect details', function () {
+
+    element(by.model('form.username')).sendKeys('incorrect');
+    element(by.model('form.password')).sendKeys('incorrect');
+    element(by.css('form .footer button')).click();
+
+    expect(browser.getTitle()).toEqual('KogoAdmin | Log in');
+
+    // error message should be displayed
+    expect(element(by.css('form .alert')).isPresent()).toBe(true);
   });
 
   it ('should allow to login with correct details', function () {
+
+    element(by.model('form.username')).clear();
+    element(by.model('form.password')).clear();
 
     element(by.model('form.username')).sendKeys('fernald.schimmel');
     element(by.model('form.password')).sendKeys('abc');
@@ -33,13 +51,4 @@ describe('KogoAdmin login screen', function () {
 
     expect(browser.getTitle()).toEqual('KogoAdmin | Dashboard');
   });
-
-  // it ('shouldn\'t allow to login with incorrect details', function () {
-
-  //   element(by.model('form.username')).sendKeys('incorrect');
-  //   element(by.model('form.password')).sendKeys('incorrect');
-  //   element(by.css('form button')).click();
-
-  //   expect(browser.getTitle()).toEqual('Kogo | Log in');
-  // });
 });
